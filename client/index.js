2773b9e2b534aacbd0339fd0c0c166c7d42f9458
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-07-03 11:35:07 +0200

    Beginning of the project - installed npm express

---
+++ client/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express(); //this const is the api server
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
