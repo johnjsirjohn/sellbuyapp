bcf395726189936b254b2466a0d2cce278a98288
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-07-03 12:12:37 +0200

    adding the files to the server folder

--- client/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const db = require('./db');
-
-const app = express(); //this const is the api server
-
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
